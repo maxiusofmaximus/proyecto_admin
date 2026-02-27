ca0b6a7a7f8e98a1fdb911634904dfd1f9a19d54
Pedro Abelardo <70430136+maxiusofmaximus@users.noreply.github.com>
2026-02-27 18:57:23 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+
+# Crear aplicación flask
+app = Flask(__name__)
+
+# ==================================
+# RUTA PRINCIPAL
+# ==================================
+
+
+# Decorador que indica la ruta principal (URL: raiz "/")
+@app.route("/")
+def inicio():
+    # Retorna y renderiza el archivo index.html
+    # que debe estar dentro de la carpeta "templates"
+    return render_template("index.html")
+
+
+# ==================================
+# RUTA USUARIOS
+# ==================================
+@app.route("/usuarios")
+def usuarios():
+    return render_template("usuarios/usuarios.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+
+# ==================================
+# EJECUTAR SERVIDOR
+# ==================================
+
+# Esta condición verifica si el archivo se está ejecutando directamente
+# y si no está siendo importado desde otro archivo
+if __name__ == "__main__":
+
+    # Inicia el servidor de Flask
+    # debug = True activa el modo desarrollador
+    # port = 5000 indica el puerto donde correrá la aplicación
+    app.run(debug=True, port=5000)
